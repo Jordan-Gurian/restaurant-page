8a921a6f06e487c38c0fdfe87bcbccadd4487150
Jordan Gurian <gurianjordan@gmail.com>
2024-03-22 15:47:09 -0700

    Finish set up of files
    Change js output name and update package and config

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,33 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
+    devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Fake Restaurant',
             template: './src/index.html',
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(otf|ttf|woff|woff2|eot)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
