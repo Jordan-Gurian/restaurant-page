5cb31c1a654fb5a6a1a0d52287435bccfb88a8b7
Jordan Gurian <gurianjordan@gmail.com>
2024-03-22 18:22:40 -0700

    Set up css styling
    CSS styling has been added for div#content

--- main.bundle.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-console.log('It worked!')
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSx5QiIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnQtcGFnZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnSXQgd29ya2VkIScpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
